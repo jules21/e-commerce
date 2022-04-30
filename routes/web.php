a4423e8be81ff9ddcd0e67cc7d551db95fbc1302
jules21 <julesfabien96@gmail.com>
2022-04-30 09:41:24 +0200

    integrate laravel ui for aunthentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware('auth')->group(function (){
+    Route::resource('/products', \App\Http\Controllers\ProductController::class);
+});
