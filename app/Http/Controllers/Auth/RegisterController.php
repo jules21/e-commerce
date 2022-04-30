7fb7677d0e420d7e92ccdb34d3ab82f92d46754e
jules21 <julesfabien96@gmail.com>
2022-04-30 14:00:57 +0200

    create default account for new user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -64,10 +65,19 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        //create default account for registered user
+
+        Account::create([
+           'user_id'=>$user->id,
+           'account_number'=>'21212-212212-' . rand(1,5000),
+        ]);
+
+        return $user;
     }
 }
